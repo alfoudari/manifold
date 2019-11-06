16bfcc61fe5101c5a841a863a165eb119b323f38
abstractpaper <aziz.alfoudari@gmail.com>
2019-11-06 23:45:49 +0300

    Added new src/dest type: stdout

---
+++ stream/stdout.go
@@ -0,0 +1,18 @@
+package stream
+
+import "fmt"
+
+type Stdout struct{}
+
+func (s *Stdout) Connect() (err error) {
+	return nil
+}
+
+func (s *Stdout) Disconnect() (err error) {
+	return nil
+}
+
+func (s *Stdout) Write(message string) (err error) {
+	_, err = fmt.Println(message)
+	return
+}
