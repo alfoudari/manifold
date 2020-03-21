2da7dbd9b82e80dcf1b78ccc73d24ecf442700b6
abstractpaper <aziz.alfoudari@gmail.com>
2020-03-22 02:40:29 +0300

    Added a test for reconnecting if conn is closed

--- stream/websocket_test.go
+++ stream/websocket_test.go
@@ -84,11 +84,40 @@ func TestWebSocket_Reconnect(t *testing.T) {
 	src.Connect()
 
 	msg, err := src.Read()
-	for i := 0; i < 100; i++ {
-		src.Write(fmt.Sprintf("echo %d", i+1))
+	for i := 0; i < 150; i++ {
+		err = src.Write(fmt.Sprintf("echo %d", i+1))
 		if err != nil {
-			log.Info("couldn't write")
-			t.Fatal(err)
+			t.Error(err)
+		}
+		fmt.Println("msg: ", <-msg)
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	log.Warn("Trying to disconnect...")
+	src.Disconnect()
+}
+
+func TestWebSocket_ReconnectConnectionClosed(t *testing.T) {
+	// log.SetLevel(log.TraceLevel5
+	// Create test server with the echo handler.
+	server := httptest.NewServer(http.HandlerFunc(echoCloseMidStream))
+	fmt.Println("Test server created")
+	defer server.Close()
+
+	src := &WebSocket{
+		URL:    "ws" + strings.TrimPrefix(server.URL, "http"),
+		Header: http.Header{},
+		Args: map[string]string{
+			"reconnect_every": strconv.Itoa(int(1 * time.Second)),
+		},
+	}
+	src.Connect()
+
+	msg, err := src.Read()
+	for i := 0; i < 150; i++ {
+		err = src.Write(fmt.Sprintf("echo %d", i+1))
+		if err != nil {
+			t.Error(err)
 		}
 		fmt.Println("msg: ", <-msg)
 		time.Sleep(10 * time.Millisecond)
@@ -112,6 +141,30 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		err = c.WriteMessage(mt, message)
 		if err != nil {
 			break
+		}
+	}
+}
+
+func echoCloseMidStream(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			break
+		}
+
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			break
+		}
+
+		if string(message) == "echo 10" {
+			c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			continue
 		}
 	}
 }
